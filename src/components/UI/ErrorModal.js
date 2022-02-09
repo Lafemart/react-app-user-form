b250fcc9f11d72c26d4abb564b2b97db02da15e6
mayowa86 <mayowasamuel86@gmail.com>
2022-02-09 17:19:02 +0100

    included modla

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,9 +1,25 @@
 
-
+import Card from "./Card";
+import Button from "./Button";
+import style from "../UI/ErrorModal.module.css";
 
 const ErrorModal = (props) => {
-return
-
+return (
+    <>
+    <div className={style.backdrop}></div>
+    <Card className={style.modal}>
+    <header className={style.header}>
+        <h2>{props.title}</h2>
+    </header>
+    <div className={style.content}>
+        <p>{props.message}</p>
+    </div>
+    <footer className={style.actions}>
+        <Button onClick={props.onConfirm}>Okay!</Button>
+    </footer>
+</Card>
+</>
+)
 
 }
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,15 +3,20 @@ import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import style from './AddUser.module.css'
-
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
     const [enteredFirstname, setEnteredFirstname] = useState('');
     const [enteredAccount, setEnteredAccount] = useState('');
+    const [error, setError] = useState();    
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if(enteredFirstname.trim().length === 0 || enteredAccount.trim().length === 0) {
+        if(enteredFirstname.trim().length === 0 || enteredAccount.trim().length < 8) {
+            setError({
+                title: 'Hey! Something went wrong!',        
+                message: 'Enter Firstname and Lastnames. Check to confrim your account number, it must be at least 11 digits'
+            });
             return;
         }
                
@@ -21,29 +26,33 @@ const AddUser = (props) => {
         
     }
 
-    const firstNameChangeHandler = (event) => {
-        event.preventDefault();
+    const firstNameChangeHandler = (event) => {        
         setEnteredFirstname(event.target.value); 
     }
 
 
-    const accountChangeHandler = (event) => {
-        event.preventDefault();
+    const accountChangeHandler = (event) => {    
         setEnteredAccount(event.target.value); 
+    };
+
+    const errorHandler = () => {
+        setError(null);
     }
 
-
     return(
+        <>
+        {error && <ErrorModal title = {error.title} message = {error.message} onConfirm={errorHandler} />}
         <Card className ={style.form}>
-        <h2>Fund support registration form</h2>
+        <h2>Support fund registration form</h2>
         <form onSubmit={addUserHandler}>    
         <label htmlFor="firstname">Fullname</label>
-        <input type="text" id="firstname" onChange={firstNameChangeHandler} value={enteredFirstname} required />       
+        <input type="text" id="firstname" onChange={firstNameChangeHandler} value={enteredFirstname} />       
         <label htmlFor="account">Account Number</label>
-        <input type="text" id="account" onChange={accountChangeHandler} value={enteredAccount} required />
+        <input type="text" id="account" onChange={accountChangeHandler} value={enteredAccount} />
         <Button type="submit">Submit Details</Button>
     </form>
     </Card>
+    </>
     )  
 };
 
